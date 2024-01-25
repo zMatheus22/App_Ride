9dc211f10fa480af9cbb4cc8bc5f97f6be9bf3b3
Matheus Vinicyus Strada <57578830+zMatheus22@users.noreply.github.com>
2024-01-25 12:10:12 -0300

    Created the `screen` and operation of the `API navigator`

---
+++ js/speedometer.js
@@ -0,0 +1,49 @@
+const speedElement = document.getElementById("speed");
+const startBtn = document.getElementById("start");
+const stopBtn = document.getElementById("stop");
+
+let watchID = null;
+
+startBtn.addEventListener("click", () => {
+  // Api do Navegador, serve para pegar as informações do dispositivo,
+  //  como a localização.
+  // Link documentação da API:
+  //  https://developer.mozilla.org/pt-BR/docs/Web/API/Window/navigator
+
+  if (watchID) return;
+
+  function handleSuccess(position) {
+    // Recebe a informação na variável `position`
+    //  para a velocidade é `{variável}.coords.speed`
+    speedElement.innerHTML = position.coords.speed
+      ? (position.coords.speed * 3.6).toFixed(1)
+      : 0;
+  }
+
+  function handleError(error) {
+    console.log(error.msg);
+  }
+
+  // Informações adicionais da API (Documentação)
+  const option = { enableHighAccuracy: true };
+  watchID = navigator.geolocation.watchPosition(
+    handleSuccess,
+    handleError,
+    option
+  );
+
+  // Remover/Adicionar os botões Start e Stop.
+  startBtn.classList.add("d-none");
+  stopBtn.classList.remove("d-none");
+});
+
+stopBtn.addEventListener("click", () => {
+  if (!watchID) return;
+
+  navigator.geolocation.clearWatch(watchID);
+  watchID = null;
+
+  // Remover/Adicionar os botões Start e Stop.
+  stopBtn.classList.add("d-none");
+  startBtn.classList.remove("d-none");
+});
